bb481efa11a87a13e4d79a9af15322bd383c07ee
Dirac <aljndr911@gmail.com>
2021-11-30 21:57:07 -0500

    yao

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Styles
-import './App.css';
+import "./App.css";
 
 // Views
-import Home from './Views/Home/Home';
-import Cart from './Views/Cart/Cart';
-import Login from './Views/Login/Login';
-import AboutUs from './Views/AboutUs/AboutUs';
-import Developers from './Views/Developers/Developers';
-import Review from './Views/Reviews/Review';
-import Register from './Views/Register/Register';
-
+import Home from "./Views/Home/Home";
+import Cart from "./Views/Cart/Cart";
+import Login from "./Views/Login/Login";
+import AboutUs from "./Views/AboutUs/AboutUs";
+import Developers from "./Views/Developers/Developers";
+import Review from "./Views/Reviews/Review";
+import Register from "./Views/Register/Register";
 
 //layOut & Context Provider
-import MainLayOut from './Customs/MainLayOut/MainLayOut';
-import { ProductsContextProvider } from './Context/ProductsContext';
-import Products from './Views/Products/Products';
+import MainLayOut from "./Customs/MainLayOut/MainLayOut";
+import { ProductsContextProvider } from "./Context/ProductsContext";
+import Products from "./Views/Products/Products";
 
 function App() {
   return (
@@ -24,16 +23,16 @@ function App() {
       <ProductsContextProvider>
         <MainLayOut>
           <Routes>
-            <Route path="/" exact element={<Home />}/>
-            <Route path="/cart"  element={ <Cart /> }/>
-            <Route path="/products"  element={ <Products /> }/>
-            <Route path="/login"  element={ <Login /> }/>
-            <Route path="/aboutus"  element={ <AboutUs /> }/>
-            <Route path="/developers" element={ <Developers />}/>
-            <Route path="/reviews" element={<Review/>}/>
-            <Route path="/register" element={<Register/>}/>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/developers" element={<Developers />} />
+            <Route path="/reviews" element={<Review />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
-          </MainLayOut>
+        </MainLayOut>
       </ProductsContextProvider>
     </BrowserRouter>
   );
--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,7 +1,7 @@
-import firebase from 'firebase/compat/app'
-import "firebase/compat/firestore"
-import 'firebase/compat/auth'
-import { getAuth, signOut } from 'firebase/auth'
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import { getAuth, signOut } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAlKiz7UT3X1fF-gTgdJQlD12iDFN3Lbas",
@@ -10,17 +10,14 @@ const firebaseConfig = {
   storageBucket: "login-ecommerce-c8694.appspot.com",
   messagingSenderId: "717071805680",
   appId: "1:717071805680:web:1beaf5d92d0d7dce546635",
-  measurementId: "G-94MM04JQ5D"
+  measurementId: "G-94MM04JQ5D",
 };
 
-firebase.initializeApp(firebaseConfig)
-const db = firebase.firestore()
-const googleProvider = new firebase.auth.GoogleAuthProvider()
-const auth = getAuth()
-const logOut = ()=>{
-  return signOut(auth)
-}
-export { db, googleProvider, firebase, logOut }
-
-
-
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+const auth = getAuth();
+const logOut = () => {
+  return signOut(auth);
+};
+export { db, googleProvider, firebase, logOut };
